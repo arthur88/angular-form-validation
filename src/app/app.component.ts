528a5928f5fff6c96372cf6832a8e7e84ed85652
Arturas <info@webai.lt>
2019-06-16 18:52:31 +0300

    angular validation form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'ngValidation';
+  angForm: FormGroup;
+  surnamePat = "^[a-zA-Z]+$";
+
+  constructor(private fb: FormBuilder) {
+    this.createForm();
+  }
+
+  createForm() {
+    this.angForm = this.fb.group({
+      name: ['', Validators.required ],
+      surname: ['', [Validators.required, Validators.minLength(5), Validators.pattern(this.surnamePat)]]
+    });
+  }
 }
